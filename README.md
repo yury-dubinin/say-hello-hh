3994149abee95e0cc9b3c9e009e8470231a904a1
yury-dubinin <yury.dubinin@gmail.com>
2022-06-03 18:17:17 +0200

    added integration test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-to run hardhat test 
-- npx hardhat test --network hardhat
+to run hh unit test 
+- npx hardhat test test/unit/SayHelloUnit.js --network hardhat
+
+to run hh integration test
+- npx hardhat test test/integration/SayHelloIntegration.js -- network rinkeby
+
+Existing contract
+- https://rinkeby.etherscan.io/address/0x0c3851bff4cd0f978e77f694ddc8ebb922122888
+- https://kovan.etherscan.io/address/0x0c3851bff4cd0f978e77f694ddc8ebb922122888
--- test/integration/SayHelloIntegration.js
+++ test/integration/SayHelloIntegration.js
@@ -1,10 +1,9 @@
 const { expect } = require('chai')
 
-describe('Say Hello new contract testing', function (){
+describe('Existing Say Hello contract Integration testing', function (){
     before(async function (){
         SayHelloContract = await ethers.getContractFactory('SayHello');
-        sayHelloContract = await SayHelloContract.deploy('Hello World!');
-        await sayHelloContract.deployed();
+        sayHelloContract = await SayHelloContract.attach('0x0C3851BFf4cD0F978e77F694DDC8EBb922122888');
     })
 
     it('should return text', async function () {
